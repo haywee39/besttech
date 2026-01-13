b992322ed01e0957ebc7e9f79f4168f9cd819334
Your Name <[haywee39@gmail.com]>
2026-01-13 17:06:29 +0100

    cat1 imgs and prices

--- product.js
+++ product.js
@@ -1,13 +1,4 @@
 // JavaScript to handle card flip on "See Details" and "Back" button clicks
-// function flipCard(event) {
-//     const card = event.target.closest('.menu-card');
-//     card.classList.add('flip');
-//   }
-  
-//   function flipBack(event) {
-//     const card = event.target.closest('.menu-card');
-//     card.classList.remove('flip');
-//   }
 
 document.addEventListener("DOMContentLoaded", () => {
 
